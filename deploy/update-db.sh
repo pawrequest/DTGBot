27dd1e16cdde9e8cd3d2350e12a0656d0f0f17b8
paw <pawrequest@users.noreply.github.com>
2024-05-18 02:02:46 +0100

    get script dir

--- deploy/update-db.sh
+++ deploy/update-db.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 docker run --rm \
-  -v "$(pwd)/../reddit.env":/app/reddit.env \
-  -v "$(pwd)/../data":/app/guru_data \
+  -v "$SCRIPT_DIR/../reddit.env":/app/reddit.env \
+  -v "$SCRIPT_DIR/../data":/app/guru_data \
   -e REDDIT_ENV=/app/reddit.env \
   -e GURU_DATA=/app/guru_data \
   dtgbot:latest update-db
