727393a686059212685614ab2fd06ed968ca261c
paw <pawrequest@users.noreply.github.com>
2024-05-18 01:17:39 +0100

    pythonpath in dockerfile env

--- deploy/update-db.sh
+++ deploy/update-db.sh
@@ -2,4 +2,6 @@
 docker run --rm \
   -v "$(pwd)/../reddit.env":/app/reddit.env \
   -v "$(pwd)/../data":/app/guru_data \
+  -e REDDIT_ENV=/app/reddit.env \
+  -e GURU_DATA=/app/guru_data \
   dtgbot:latest update-db
